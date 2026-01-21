2f6c0b3dbf095c02fce0a19dfc73be2d4d61d4b6
amit-kumar8-sf <amit.kumar.sf1408@gmail.com>
2026-01-22 02:01:45 +0530

    @W-20893693: Add AM topic with user and role command

--- packages/b2c-tooling-sdk/test/cli/user-command.test.ts
+++ packages/b2c-tooling-sdk/test/cli/user-command.test.ts
@@ -8,7 +8,7 @@ import {expect} from 'chai';
 import sinon from 'sinon';
 import {Config} from '@oclif/core';
 import {UserCommand} from '@salesforce/b2c-tooling-sdk/cli';
-import {isolateConfig, restoreConfig} from '../helpers/config-isolation.js';
+import {isolateConfig, restoreConfig} from '@salesforce/b2c-tooling-sdk/test-utils';
 import {stubParse} from '../helpers/stub-parse.js';
 
 // Create a test command class
