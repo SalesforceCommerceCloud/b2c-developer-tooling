8f31cecbbc4a84ea2e110e50d142e145b0be6eef
Kieran Haberstock <khaberstock@salesforce.com>
2026-02-26 11:15:58 -0800

    Add mrt-utilities

---
+++ packages/mrt-utilities/src/streaming/index.ts
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2025, Salesforce, Inc.
+ * SPDX-License-Identifier: Apache-2
+ * For full license text, see the license.txt file in the repo root or http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+export const createNullStream = () => {};
+export {
+  type CompressionConfig,
+  createExpressRequest,
+  createExpressResponse,
+  createStreamingLambdaAdapter,
+} from './create-lambda-adapter.js';
