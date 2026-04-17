bc0e4b3b86d8481413c97be77d9d610f52d158f6
Ben Chypak <bchypak@mobify.com>
2026-04-17 09:12:48 -0700

    Initial Commit (#349)

--- packages/mrt-utilities/src/middleware/index.ts
+++ packages/mrt-utilities/src/middleware/index.ts
@@ -4,6 +4,5 @@
  * For full license text, see the license.txt file in the repo root or http://www.apache.org/licenses/LICENSE-2.0
  */
 
-export * from './data-store.js';
-export * from './middleware.js';
-export {type ProxyConfig} from '../utils/configure-proxying.js';
+export * from '../data-store/index.js';
+export * from './express.js';
